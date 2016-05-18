bf50f9a9eb7c9529d3366f37984c0689ff5c3d0b
oredavids <oredavids@gmail.com>
2016-05-18 09:06:30 +0100

    create a function search

--- Notes Application.js
+++ Notes Application.js
@@ -24,3 +24,39 @@ function notesApplication(author){
 			return "Does not exist!";
 		}
 	}
+
+	this.search = function(search_text){
+		
+		var arrIndex = [];
+		var regularExpression = new RegExp(search_text,"i");
+		
+		for(let j=0;j<this.notes.length;j++)
+		{
+			
+			if(regularExpression.test(this.notes[j]))
+			{
+				arrIndex.push(j);
+				//console.log(j);
+			}
+		}
+
+
+		var searchFormat = "Showing results for search ‘[ "+search_text+" ]’\n";
+
+
+		if(arrIndex.length>0)
+		{	
+			for(let i=0;i<arrIndex.length;i++)
+			{
+				searchFormat += "Note ID: "+arrIndex[i]+"\n";
+				searchFormat += this.notes[arrIndex[i]]+"\n";
+				searchFormat += "By Author"+this.author+"\n\n\n";
+			}
+		}
+		else
+		{
+			searchFormat += "No Such string exists in the application";
+		}
+		
+		return searchFormat;
+	}
